4a80a715e9579d8c582eefbab1f3e006e913624f
xMorioh <lukas.boeckelmann.lb@gmail.com>
2024-06-13 02:59:19 +0200

    Updated Reencode_video_to_specific_filesize added further optimizations which should greatly improve encoding times for the same quality output

--- Scripts/Windows/ffmpeg/Reencode_video_to_specific_filesize/Reencode_video_to_specific_filesize.ps1
+++ Scripts/Windows/ffmpeg/Reencode_video_to_specific_filesize/Reencode_video_to_specific_filesize.ps1
@@ -7,7 +7,7 @@ $ffprobePath = "$PSScriptRoot\ffprobe.exe"
 $inputVideo = "A:\input.*" #No need to specify the file extension
 $outputPath = "A:\output.mp4"
 $targetVideoSize_megabytes = 50
-$encoder = "libvpx-vp9" #Choose Encoder Library, for example x264=libx264 or VP9=libvpx-vp9
+$encoder = "libvpx-vp9" #Choose your prefered Encoder Library, for example x264=libx264, VP9=libvpx-vp9, AV1=libsvtav1
 
 #-----Script internals-----
 $inputVideo = Get-ChildItem -Path $inputVideo #search for file extension via input name
@@ -33,12 +33,30 @@ Write-Host "targetFileSize_kbit                   :" $targetFileSize_kbit
 Write-Host "OutputBitrate_kbit before Optimization:" $OutputBitrate_kbit
 
 #-----Optimizations-----
-if ($OutputBitrate_kbit -gt $probedFileVideoBitrate_kbit) {
-$OutputBitrate_kbit = $probedFileVideoBitrate_kbit
-}
-if ($OutputBitrate_kbit -gt 20000) {
-$OutputBitrate_kbit = 20000
-}
+#Limit kbit to the original videos kbit if it's lower than our calculation since wizards do not actually exist and the gain would be too small if any.
+    if ($OutputBitrate_kbit -gt $probedFileVideoBitrate_kbit) {
+        $OutputBitrate_kbit = $probedFileVideoBitrate_kbit
+    }
+    
+#--Check if we can use libx264 instead to save on encoding time if the exact same quality can be achieved in our targetVideoSize, we do not change $OutputBitrate_kbit here we still use target max kbit.
+    [int]$probedFileVideoWidth = Invoke-Expression "&'$ffprobePath' -v error -select_streams v:0 -show_entries stream=width -of default=noprint_wrappers=1:nokey=1 '$inputVideo'"
+    [int]$probedFileVideoHeight = Invoke-Expression "&'$ffprobePath' -v error -select_streams v:0 -show_entries stream=height -of default=noprint_wrappers=1:nokey=1 '$inputVideo'"
+    [int]$probedFileVideoPixels = ($probedFileVideoWidth * $probedFileVideoHeight)
+    [string]$probedFileVideoFPS = Invoke-Expression "&'$ffprobePath' -v error -select_streams v:0 -show_entries stream=r_frame_rate -of default=noprint_wrappers=1:nokey=1 '$inputVideo'"
+    $probedFileVideoFPSMultiplier = [Data.DataTable]::New().Compute($probedFileVideoFPS, $null) / 30
+    #Info: 0.00125 = ((((Baseline kbit (500kbit) / 1280*720 assuming the 500Kkbit are good for a 30fps video at that resolution) * 2) + 10%) to get a base quality number meaning kbit for each pixel per second @30fps
+    #*2 + 10% = x2 is for the x264 codec uplift since VP9 has around 50% better quality compared and then just 10% on top for good measure + rounding up, $probedFileVideoFPS right below here is for 30fps to 60fps when 500kbit look good at 30fps.
+    $x264Check_kbit = (0.00125 * $probedFileVideoFPSMultiplier) * $probedFileVideoPixels
+    $x264Check_megabytes = (((($x264Check_kbit * $probedFileDuration) / 8192) + 1) + $AudioSize_megabytes)
+    if ($x264Check_megabytes -lt $targetVideoSize_megabytes) {
+        $encoder = "libx264"
+    }
+#--Check end
+
+#Limit kbit for user set encoder, this way we can get away with a much lower filesize in some scenarios
+    elseif ($OutputBitrate_kbit -gt 10000) {
+        $OutputBitrate_kbit = 10000
+	}
 #-----------------------
 
 Write-Host "OutputBitrate_kbit after Optimization :" $OutputBitrate_kbit
