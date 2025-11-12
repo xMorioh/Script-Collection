2b41453d9a767be52e3ed058ce133274b6d71635
Morioh <Morioh@DESKTOP-K6G589C>
2025-11-12 18:05:54 +0100

    Updated PresentMon Graph Plotter to skip the first 200 and last 100 samples for driver optimization bypass where the game was not in focus by the time of recording

--- Scripts/Windows/PresentMon/Quick_Launcher/PresentMon_QuickLauncher.bat
+++ Scripts/Windows/PresentMon/Quick_Launcher/PresentMon_QuickLauncher.bat
@@ -1,5 +1,5 @@
 @echo off
 set /p "pName=Enter Process Name: "
-set scriptPath=%~dp0
+set exePath=C:\Program Files\Intel\PresentMon\PresentMonConsoleApplication\
 for /f "skip=1" %%a in ('forfiles /M PresentMon*.exe') do set exeFile=%%a
-Start "[PresentMon] Watching Process: %pName%" "%scriptPath%%exeFile%" --process_name "%pName%" --terminate_on_proc_exit --date_time --exclude_dropped
+Start "[PresentMon] Watching Process: %pName%" "%exePath%%exeFile%" --process_name "%pName%" --terminate_on_proc_exit --exclude_dropped
